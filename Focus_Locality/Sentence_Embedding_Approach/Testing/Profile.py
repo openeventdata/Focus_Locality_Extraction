c41e317f6d0ffa1c29e5db9c6b9f59ef02fb8e93
Maryam Imani <imani.ma@gmail.com>
2018-10-11 13:01:14 -0500

    Update Profile.py

--- Focus_Locality/Sentence_Embedding_Approach/Testing/Profile.py
+++ Focus_Locality/Sentence_Embedding_Approach/Testing/Profile.py
@@ -8,15 +8,13 @@ import SentenceExtraction_test
 from itertools import izip
 
 
-def main(lang, TextTest, word2vec_Dictionary, loaded_model):
+def main(lang, NER, TextTest, word2vec_Dictionary, loaded_model):
 
+#### test
 #    lang = 'en'
-#    TextTest = sys.argv[1]
 #    TextTest = '''105 words 4 July 2014 03:44 All Africa AFNWS English Mogadishu, Jul 04, 2014 (Tunis Afrique Presse/All Africa Global Media via COMTEX) -- A member of the Somali Federal Parliament has been shot dead by unknown gunmen on Thursday morning in Mogadishu, officials said. Ahmed Mohamud Hayd was killed in a drive-by shooting after he left his hotel in a heavily policed area, witnesses said. His bodyguard was also killed and a parliamentary secretary wounded in the shooting. Al-Shabab spokesman Abdulaziz Abu Musab said the group had carried out the "targeted assassination". At least five members of the Parliament have been shot since the beginning of the year.  '''
-
-    NER = 'Mitie' #Stanford or Mitie or other
+#    NER = 'Mitie' #Stanford or Mitie or other
         
-    # test
     SentenceListTest, LocTest = SentenceExtraction_test.main(TextTest, NER, lang)
     embTest = SIFpreprocessing_test.main(0.01, lang, TextTest, word2vec_Dictionary)
     
